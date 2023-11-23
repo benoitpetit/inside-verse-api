fd94559d3f734ee7515081474288f017b0644bf1
ben <p6xmjy6ji@mozmail.com>
2023-11-23 18:04:23 +0100

    make it better 2

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 
-server.use((req, res, next) => {
-  if (req.method === "GET") {
-    next();
-  } else {
-    res.sendStatus(405);
-  }
-});
+// server.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     next();
+//   } else {
+//     res.sendStatus(405);
+//   }
+// });
 
 server.use(router);
 
